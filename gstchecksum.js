08966217a22f8279f4f93655c6526efa0760d098
Arjunkumar <tk120404@users.noreply.github.com>
2019-02-01 17:15:15 +0530

    Update gstchecksum.js

--- gstchecksum.js
+++ gstchecksum.js
@@ -1,12 +1,11 @@
-function checksum(g)
-{
+function checksum(g){
     let a=65,b=55,c=36;
     return Array['from'](g).reduce((i,j,k,g)=>{ 
-    p=(p=(j.charCodeAt(0)<a?parseInt(j):j.charCodeAt(0)-b)*(k%2+1))>c?1+(p-c):p 
-    return k<14?i+p:j==String.fromCharCode((c-(i%c))+b);
+    p=(p=(j.charCodeAt(0)<a?parseInt(j):j.charCodeAt(0)-b)*(k%2+1))>c?1+(p-c):p;
+    return k<14?i+p:j==((c=(c-(i%c)))<10?c:String.fromCharCode(c+b));
     },0);     
 }
 
-
-// console.log(checksum('27AASCS2460H1Z0')) //false
-// console.log(checksum('27AAPFU0939F1ZV')) //true
+//console.log(checksum('27AAPFU0939F1ZV')) //true
+//console.log(checksum('27AASCS2460H1Z0')) //false
+//console.log(checksum('29AAGCB7383J1Z4')) //true
