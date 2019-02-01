bb552250d38fa8baa7f847ba74c11cc805805046
Jayanth07 <jayanth.saisurya@gmail.com>
2019-02-01 16:10:44 +0530

    Update GST_checksum.py

--- GST_checksum.py
+++ GST_checksum.py
@@ -4,6 +4,6 @@ def checksum(gst):
     l = [int(c) if c.isdigit() else ord(c)-55 for c in gst]
     l = [val*(ind % 2 + 1) for (ind, val) in list(enumerate(l))]
     l = [(int(x/36) + x%36) for x in l]
-    csum = (36 - int(sum(l)/36))
+    csum = (36 - sum(l)%36)
     csum = str(csum) if (csum < 10) else chr(csum + 55)
     return True if (check == csum) else False
