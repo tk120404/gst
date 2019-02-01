9f3be001eaeb165cf9ddfd7a2fedc80ced9b7ed8
Jayanth07 <jayanth.saisurya@gmail.com>
2019-02-01 15:35:04 +0530

    Add files via upload

---
+++ GST_checksum.py
@@ -0,0 +1,9 @@
+def checksum(gst):
+    check = gst[-1]
+    gst = gst[:-1]
+    l = [int(c) if c.isdigit() else ord(c)-55 for c in gst]
+    l = [val*(ind % 2 + 1) for (ind, val) in list(enumerate(l))]
+    l = [(int(x/36) + x%36) for x in l]
+    csum = (36 - int(sum(l)/36))
+    csum = str(csum) if (csum < 10) else chr(csum + 55)
+    return True if (check == csum) else False
